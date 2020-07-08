4d6bf830b7f8085e9b3c964730f46e605138939b
Crowdbotics <team@crowdbotics.com>
2020-07-08 12:11:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vibing_messages_18716/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vibing_messages_18716;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "vibing_messages_18716";
+    }
+}
